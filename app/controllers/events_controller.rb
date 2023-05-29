fb52d77f9999ee416ebd3e992d8c63f7d59c2e1f
hrk-0830 <ponte.di.ingresso11@gmail.com>
2023-05-29 17:13:12 +0000

    EventView[編集]

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,46 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
+      if @event.save
+        # タグの保存
+        redirect_to event_path(@event.id)
+        flash[:announce] = "投稿に成功しました"
+      else
+        flash[:danger] = "必要情報を入力してください"
+        render :new
+      end
   end
 
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :overview, :prefecture_status, :start_time, :user_id, :image)
+  end
+
+
+  def ensure_correct_customer
+    @post = Post.find(params[:id])
+    @customer = @post.customer
+    unless @customer == current_customer
+      flash[:danger] = "他のユーザーの情報は編集できません。"
+      redirect_to public_posts_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'events/new'
-  get 'events/index'
-  get 'events/show'
-  get 'events/edit'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
+  resources :events, only: [:new,:index,:show,:edit,:create,:destroy,:update] do
+    resources :comments,only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  root to: 'events#index'
+  resources :users, only: [:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
